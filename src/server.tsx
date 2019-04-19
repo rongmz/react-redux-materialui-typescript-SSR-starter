cb1be00224d5422140dda20cbd47cf076f8be42f
Rounak Saha <somarongames@gmail.com>
2019-04-19 20:27:15 +0530

    Initial Commit. Includes React, server-side rendering (razzle), material-ui, sample code

---
+++ src/server.tsx
@@ -0,0 +1,56 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+
+import App from './App';
+
+let assets: any;
+
+const syncLoadAssets = () => {
+  assets = require(process.env.RAZZLE_ASSETS_MANIFEST!);
+};
+syncLoadAssets();
+
+const server = express()
+  .use(function (req, res, next) {    //----------------
+    res.removeHeader("x-powered-by"); // Remove X-Powered-By
+    next();                           // Header from response.
+  })                                  //----------------
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
+  .get('/*', (req: express.Request, res: express.Response) => {
+    const context = {};
+    const markup = renderToString(
+      <StaticRouter context={context} location={req.url}>
+        <App />
+      </StaticRouter>
+    );
+    res.send(
+      `<!doctype html>
+    <html lang="">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet='utf-8' />
+        <title>App Maker</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${
+          assets.client.css
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''
+        }
+          ${
+            process.env.NODE_ENV === 'production'
+              ? `<script src="${assets.client.js}" defer></script>`
+              : `<script src="${assets.client.js}" defer crossorigin></script>`
+          }
+    </head>
+    <body>
+        <div id="root">${markup}</div>
+    </body>
+</html>`
+    );
+  });
+
+export default server;
